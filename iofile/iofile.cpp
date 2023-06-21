c31805d4501da446120996e152c32fc1d71f6a08
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-06-21 10:48:50 +0700

    jika file tidak ditemukan file akan menampilkan 'unable to open file'

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -43,7 +43,17 @@ int main() {
 	if (infile.is_open())
 	{
 		//melakukan perulangan setiap baris
-		while (getline(infile, baris)) 
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
 	}
+
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 
 }
